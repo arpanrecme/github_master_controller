34a4bf93463e4c975a22cdf10f8c3fb67f46c483
arpan <arpan.rec@gmail.com>
2022-12-05 23:39:24 +0530

    ansible_lint

--- terraform/arpanrec/locals.tf
+++ terraform/arpanrec/locals.tf
@@ -1,6 +1,5 @@
 locals {
   PYTHON_REQUIRED_STATUS_CHECKS_CONTEXTS           = ["pylint", "docker"]
-  ANSIBLE_ROLE_REQUIRED_STATUS_CHECKS_CONTEXTS     = ["molecule_test"]
   ANSIBLE_PLAYBOOK_REQUIRED_STATUS_CHECKS_CONTEXTS = ["ansible_lint"]
   ANSIBLE_REQUIRED_STATUS_CHECKS_CONTEXTS          = ["ansible_lint", "molecule_test"]
   TERRAFORM_REQUIRED_STATUS_CHECKS_CONTEXTS        = ["terraform_lint", "terraform_plan", "terraform_dryrun", "terraform_validate"]
