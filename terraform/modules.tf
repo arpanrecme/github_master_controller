7e362ff65cf4f677893f1c2ed9ffbcf088b4ec94
arpan <arpan.rec@gmail.com>
2022-12-24 09:05:54 +0530

    fmt

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -1,7 +1,7 @@
 module "arpanrec" {
   source = "./arpanrec"
 
-  license_template                        = var.license_template
-  repository_default_branch = var.repository_default_branch
-  project_topic_controlled_by_master      = var.project_topic_controlled_by_master
+  license_template                   = var.license_template
+  repository_default_branch          = var.repository_default_branch
+  project_topic_controlled_by_master = var.project_topic_controlled_by_master
 }
